2e645b07891e0dad26a671107922ab3cc41b9322
dmtrack <dmtrack.dev@gmail.com>
2023-05-08 14:48:21 +0500

    feature: new item card style

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,73 +1,80 @@
-import { useState } from 'react';
-
 import { Box, Typography, useTheme, Button } from '@mui/material';
 import { shades } from '../theme';
 import { useNavigate } from 'react-router-dom';
-import { IItem, IItemProps } from '../models/IItem';
+import { IItemProps } from '../models/IItem';
+import { ThemeChip } from './ThemeChip';
+import { useAppSelector } from '../hook/redux';
+import { ICollection } from '../models/ICollection';
 
 const Item: React.FC<IItemProps> = ({ item, width }: IItemProps) => {
     const navigate = useNavigate();
-    const collectionName = 'collection_name';
+    const { collections } = useAppSelector((state) => state.collections);
 
-    const [isHovered, setIsHovered] = useState(false);
+    const currentItemCollection: ICollection | undefined = collections.find(
+        (c) => Number(c.id) === Number(item.collectionId)
+    );
+    const themeId = currentItemCollection?.themeId;
+
     const {
         palette: { neutral },
     } = useTheme();
-    const { name, image, id } = item;
+    const { name, image, id, collectionId } = item;
 
     return (
         <Box width={width}>
-            <Box
-                position="relative"
-                onMouseOver={() => setIsHovered(true)}
-                onMouseOut={() => setIsHovered(false)}
-            >
+            <Box position='relative'>
                 <img
                     alt={item.name}
-                    width="300px"
-                    height="400px"
+                    width='300px'
+                    height='400px'
                     src={image}
                     onClick={() => navigate(`item/${id}`)}
                     style={{ cursor: 'pointer', borderRadius: '5px' }}
                 />
                 <Box
-                    display={isHovered ? 'block' : 'none'}
-                    position="absolute"
-                    bottom="10%"
-                    left="0"
-                    width="100%"
-                    padding="0 5%"
-                >
-                    <Box display="flex" justifyContent="space-between">
-                        <Box
-                            display="flex"
-                            alignItems="center"
-                            borderRadius="3px"
-                            // sx={{ backgroundColor: shades.neutral[100] }}
-                        >
-                            <Typography color="white">{item.name}</Typography>
+                    position='absolute'
+                    bottom='5%'
+                    left='0'
+                    width='100%'
+                    height='90%'
+                    padding='0 5%'>
+                    <Box
+                        display='flex'
+                        flexDirection='column'
+                        justifyContent='space-between'
+                        height='100%'>
+                        <Box display='flex'>
+                            {' '}
+                            <ThemeChip
+                                themeId={Number(themeId)}
+                                color='default'
+                                backgroundColor='white'
+                            />
                         </Box>
-                        <Box></Box>
-                        <Button
-                            onClick={() => navigate(`item/${id}`)}
-                            sx={{
-                                backgroundColor: shades.primary[300],
-                                color: 'white',
-                            }}
-                        >
-                            GO
-                        </Button>
+                        <Box display='flex' justifyContent='space-between'>
+                            <Box
+                                display='flex'
+                                alignItems='center'
+                                borderRadius='3px'
+                                // sx={{ backgroundColor: shades.neutral[100] }}
+                            >
+                                <Typography color='white'>
+                                    {item.name}
+                                </Typography>
+                            </Box>
+
+                            <Button
+                                onClick={() => navigate(`item/${id}`)}
+                                sx={{
+                                    backgroundColor: shades.primary[100],
+                                    color: shades.secondary[800],
+                                }}>
+                                GO
+                            </Button>
+                        </Box>
                     </Box>
                 </Box>
             </Box>
-            {/* <Box mt="3px">
-                <Typography variant="subtitle2" color={neutral.dark}>
-                    {collectionName
-                        .replace(/([A-Z]/g, ' $1')
-                        .replace(/^./, (str) => str.toUpperCase())}
-                </Typography>
-                <Typography>{name}</Typography>
-            </Box> */}
         </Box>
     );
 };
